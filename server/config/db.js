e0e6c5888b58bf3ac0980fb8c916c55b8f33308e
ADHISH PK <adhish9955@gmail.com>
2026-07-04 16:13:05 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,6 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "Admin@123",
-  database: "fintrack",
 });
 
 module.exports = db;
